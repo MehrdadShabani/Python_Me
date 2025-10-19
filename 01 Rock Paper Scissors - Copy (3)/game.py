c9aa9848c89a4348fd327e85fbbba12848fbd63e
Mehrdad Shabani <shabani2696@gmail.com>
2025-10-19 09:17:36 +0330

    Auto backup on Sun 10/19/2025  9:17:36.70

---
+++ 01 Rock Paper Scissors - Copy (3)/game.py
@@ -0,0 +1,22 @@
+import random
+from typing import List
+
+
+class RockPaperScissors:
+    """
+    Main class for the game Rock, Paper, Scissors
+    """
+    def __init__(self):
+        self.choices: List[str] = ['rock', 'paper', 'scissors']
+
+    def get_user_choice(self) -> str:
+        """Method to get the user's choice.
+        
+        :return: User's choice as a string
+        """
+
+        user_choice: str = input("Enter your choice (rock/paper/scissors): ")
+        if user_choice in self.choices:
+            return user_choice
+        else:
+            print    
